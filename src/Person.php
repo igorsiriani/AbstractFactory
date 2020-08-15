47d759ba29901488b83f440b1213a856c9a3a03c
Igor <igorsiriani@gmail.com>
2020-08-15 05:20:35 -0300

    initial commit

---
+++ src/Person.php
@@ -0,0 +1,10 @@
+<?php
+
+interface Person {
+
+    public function getName() : string;
+    public function getDocument() : string;
+    public function randomName() : string;
+    public function ramdomDocument() : string;
+
+}
